a1f2bfce863443a53985a91bd17a42c1c73b480a
Lyarkh <lcemanuel.emanuel@gmail.com>
2021-12-16 10:19:33 -0300

    utilizando design patterns Strategy.

--- src/calculador_de_impostos.py
+++ src/calculador_de_impostos.py
@@ -1,15 +1,10 @@
 from orcamento import Orcamento
-from impostos import calcula_ISS, calcula_ICMS 
+from impostos import ISS, ICMS 
 
 class CalculadoraImpostos(object):
 
     def realiza_calculo(self, orcamento, imposto):
-        imposto = imposto.upper()
-
-        if imposto == "ISS":
-            imposto_calculado = calcula_ISS(orcamento)
-        if imposto == "ICMS":
-            imposto_calculado = calcula_ICMS(orcamento)
+        imposto_calculado = imposto.calcula(orcamento)
 
         print (imposto_calculado)
 
@@ -17,5 +12,5 @@ if __name__ == "__main__":
     calculador = CalculadoraImpostos()
     orcamento = Orcamento(500)
 
-    calculador.realiza_calculo(orcamento, "iss")
-    calculador.realiza_calculo(orcamento, "icms")
+    calculador.realiza_calculo(orcamento, ISS())
+    calculador.realiza_calculo(orcamento, ICMS())
--- src/impostos.py
+++ src/impostos.py
@@ -1,6 +1,10 @@
 
-def calcula_ISS(orcamento):
-    return orcamento.valor * 0.1
+class ISS(object):
+    def calcula(self, orcamento):
+        return orcamento.valor * 0.1
 
-def calcula_ICMS(orcamento):
-    return orcamento.valor * 0.06
+
+class ICMS:
+
+    def calcula(self, orcamento):
+        return orcamento.valor * 0.06
