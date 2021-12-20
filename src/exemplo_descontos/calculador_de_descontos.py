137fd1caaef46513bba3225dc2e5657d3b4ff5f0
Lyarkh <lcemanuel.emanuel@gmail.com>
2021-12-20 09:46:32 -0300

    Utilizando padrão chain of responsibility.

--- src/exemplo_descontos/calculador_de_descontos.py
+++ src/exemplo_descontos/calculador_de_descontos.py
@@ -1,21 +1,17 @@
-from exemplo_descontos.orcamento import Orcamento, Item
+from orcamento import Orcamento, Item
+from descontos import DescontoPorCincoItens, DescontPorMaisDeQuinhentosReais, SemDesconto
 
 class CalculadoraDescontos(object):
 
     def calcula(self, orcamento):
-
-        if orcamento.total_itens > 5:
-            return orcamento.valor * 0.1
-        elif orcamento.valor > 500:
-            return orcamento.valor * 0.07
-
+        desconto = DescontoPorCincoItens(DescontPorMaisDeQuinhentosReais(SemDesconto)).calcula(orcamento)
+        return desconto
+        
 if __name__ == "__main__":
     orcamento = Orcamento()
     orcamento.adiciona_item(Item("Item - 01", 100))
     orcamento.adiciona_item(Item("Item - 01", 50))
     orcamento.adiciona_item(Item("Item - 01", 400))
-
-    print(orcamento.valor)
 
     calculador = CalculadoraDescontos()
     desconto_calculado = calculador.calcula(orcamento)
