06d89065d4a6643f912f6c1e607d3c70ba920352
Lyarkh <lcemanuel.emanuel@gmail.com>
2021-12-20 10:29:49 -0300

    Template Method e organizando o projeto.

--- src/exemplo_impostos/impostos.py
+++
@@ -1,9 +0,0 @@
-
-class ISS(object):
-    def calcula(self, orcamento):
-        return orcamento.valor * 0.1
-
-class ICMS:
-
-    def calcula(self, orcamento):
-        return orcamento.valor * 0.06
