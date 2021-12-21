39fdc4657249c4e8839987ff33a80cc7f8732130
Lyarkh <lcemanuel.emanuel@gmail.com>
2021-12-21 17:03:11 -0300

    Design Pattern Command

---
+++ src/Modulo_02/Aula_05/pedido.py
@@ -0,0 +1,76 @@
+from abc import abstractmethod, ABCMeta
+from datetime import date
+
+class Pedido(object):
+    def __init__(self, cliente, valor):
+        self.__cliente = cliente
+        self.__valor = valor
+        self.__status = 'NOVO'
+        self.__data_finalizacao = None
+
+    def paga(self):
+        self.__status = 'PAGO'
+
+    def finaliza(self):
+        self.__data_finalizacao = date.today()
+        self.__status = 'ENTREGUE'
+
+    @property
+    def cliente(self):
+        return self.__cliente
+
+    @property
+    def valor(self):
+        return self.__valor
+
+    @property
+    def status(self):
+        return self.__status
+
+    @property
+    def data_finalizacao(self):
+        return self.__data_finalizacao
+
+class Comando(metaclass=ABCMeta):
+
+    @abstractmethod
+    def executa(self):
+        pass
+
+class ConcluiPedido(Comando):
+    def __init__(self, pedido):
+        self.__pedido = pedido
+    
+    def executa(self):
+        self.__pedido.finaliza()
+
+class PagaPedido(Comando):
+    def __init__(self, pedido):
+        self.__pedido = pedido
+    
+    def executa(self):
+        self.__pedido.paga()
+
+class FilaDeTrabalho(object):
+    def __init__(self):
+        self.__comandos = []
+    
+    def adiciona(self, comando):
+        self.__comandos.append(comando)
+
+    def processa(self):
+        for comando in self.__comandos:
+            comando.executa()
+
+if __name__ == "__main__":
+
+    pedido1 = Pedido("Flavio", 200)
+    pedido2 = Pedido("Almeida", 400)
+
+    fila_de_trabalho = FilaDeTrabalho()
+    fila_de_trabalho.adiciona(ConcluiPedido(pedido1))
+    fila_de_trabalho.adiciona(PagaPedido(pedido1))
+    fila_de_trabalho.adiciona(ConcluiPedido(pedido2))
+
+    fila_de_trabalho.processa()
+
